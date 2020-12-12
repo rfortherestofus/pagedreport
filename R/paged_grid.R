bcb7ed0b59d3566d0f0ba0f13591f277f2bd0183
Thomas Vroylandt <tvroylandt@gmail.com>
2020-12-12 09:29:09 +0100

    Adding grid function

--- R/paged_grid.R
+++ R/paged_grid.R
@@ -1,3 +1,29 @@
-paged_grid <- function(){
+#' Function for grid template
+#'
+#' @param logo Logo
+#'
+#' @return A pagedown template
+#' @export
+#'
+paged_grid <- function(logo = "0"){
 
+  # arguments
+  main_css <-
+    pkg_resource("css/style_grid.css")
+  pandoc_html <-
+    pkg_resource("html/template_grid.html")
+
+  # default
+  if (logo == "0") {
+    logo <-
+      pkg_resource("logo/square-logo.svg")
+  }
+
+  # template
+  pagedown::html_paged(
+    css = main_css,
+    template = pandoc_html,
+    front_cover = logo,
+    back_cover = logo
+  )
 }
