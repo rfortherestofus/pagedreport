0b1a67c19b1b1edcd61b24900983ef2c7c99c41f
gorantesj <gorantesj@hotmail.com>
2022-01-21 12:16:07 -0600

    Prueba 1

---
+++ R/paged_morant.R
@@ -0,0 +1,34 @@
+#' Function for Morant template
+#'
+#' @param logo Logo
+#' @param other_css Add an other CSS
+#' @param ... Arguments passed to pagedown::html_paged
+#'
+#' @return A pagedown template
+#' @export
+#'
+paged_grid <- function(logo = "0",
+                       other_css = NULL,
+                       ...){
+
+  # arguments
+  main_css <-
+    pkg_resource("css/style_morant.css")
+  pandoc_html <-
+    pkg_resource("html/template_paged.html")
+
+  # default
+  if (logo == "0") {
+    logo <-
+      pkg_resource("logo/square-logo.svg")
+  }
+
+  # template
+  pagedown::html_paged(
+    css = c(other_css, main_css),
+    template = pandoc_html,
+    front_cover = logo,
+    back_cover = logo,
+    ...
+  )
+}
